3761e176fa9c6af6564bd3b73aa9075144c86ddc
XL <XL@qq.com>
2021-12-10 19:42:07 +0800

    second commit

--- src/main/java/com/ln/git/GitTest.java
+++ src/main/java/com/ln/git/GitTest.java
@@ -7,5 +7,6 @@ package com.ln.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!22222222");
     }
 }
