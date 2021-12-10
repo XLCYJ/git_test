558851c86db1ab4eca3476038bce176c24cf04ed
XL <XL@qq.com>
2021-12-10 19:33:25 +0800

    first commit

---
+++ src/main/java/com/ln/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ln.git;
+
+/**
+ * @author shkstart
+ * @create 10 - 19:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
