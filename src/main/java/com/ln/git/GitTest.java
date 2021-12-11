63ae36f86d1c9aa57ebd9e87448d3caa3c17ceaa
XL <XL@qq.com>
2021-12-11 08:36:20 +0800

    push commit

--- src/main/java/com/ln/git/GitTest.java
+++ src/main/java/com/ln/git/GitTest.java
@@ -11,7 +11,7 @@ public class GitTest {
         System.out.println("Hello Git!333333333");
         System.out.println("hot-fix test");
         System.out.println("master test222222");
-
+        System.out.println("push test");
 
     }
 }
